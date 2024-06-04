f480c19155e8f0c3a847493ede732d99aafd89e6
JuanSobalvarro <sobalvarrog.juans@gmail.com>
2024-06-04 13:28:46 -0600

    Unregistering models in admin

--- SGD/core_application/admin.py
+++ SGD/core_application/admin.py
@@ -1,10 +1,1 @@
 from django.contrib import admin
-from .models import PlayerInfo, Sport, Team, Record, Player, Match, Tournament
-
-admin.site.register(PlayerInfo)
-admin.site.register(Sport)
-admin.site.register(Team)
-admin.site.register(Record)
-admin.site.register(Player)
-admin.site.register(Match)
-admin.site.register(Tournament)
