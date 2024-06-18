07b3c103b652eca0bd7eba4651be151d61a65310
JuanSobalvarro <sobalvarrog.juans@gmail.com>
2024-06-18 13:52:26 -0600

    TableTennis models started

--- SGD/sports/tableTennis/models.py
+++ SGD/sports/tableTennis/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from ...core_application import models as core_models
 
-# Create your models here.
+
+class TableTennisPlayerInfo(core_models.PlayerInfo):
+    # attributes to override
+    class Meta:
+        app_label = 'tableTennis'
+        verbose_name = 'Table Tennis Player Info'
+
+    def __str__(self):
+        return self.name
