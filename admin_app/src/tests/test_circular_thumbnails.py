5cb8a5de8e1c68e9da95193e3349c49f17a0db5c
JuanSobalvarro <sobalvarrog.juans@gmail.com>
2024-07-23 15:17:01 -0600

    Working on admin_app: circular images crop #12

--- admin_app/src/tests/test_circular_thumbnails.py
+++ admin_app/src/tests/test_circular_thumbnails.py
@@ -3,10 +3,10 @@ from admin_app.src.utils.utils import create_circular_image
 
 def main():
     # Path to your image
-    image_path = "../images/test/test-96.png"
+    image_path = "../images/test/testkkck.png"
 
     # Create a circular image with a specified background color
-    circular_image = create_circular_image(image_path, size=1000, circle_pad=100, bg_color=(255, 0, 0))  # Red background
+    circular_image = create_circular_image(image_path, size=1000)  # Red background
     circular_image.show()
 
 
