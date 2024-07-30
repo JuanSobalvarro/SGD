e80c3a3dcbd16c99f7fcb628f08ea6eaf2da727c
JuanSobalvarro <sobalvarrog.juans@gmail.com>
2024-07-30 16:10:12 -0600

    Working on views core_application

--- SGD_django/core_application/views.py
+++ SGD_django/core_application/views.py
@@ -1,11 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
 import datetime
 
 
 def home(request):
-
+    """
+    View function for home page
+    """
     if request.method == 'POST':
         return render(request, 'core_application/home.html')
 
@@ -13,3 +14,15 @@ def home(request):
         return render(request, 'core_application/home.html')
 
     return render(request, 'core_application/home.html')
+
+
+def about(request):
+    """
+    View function for about page
+    """
+    if request.method == 'POST':
+        return render(request, 'core_application/about.html')
+    elif request.method == 'GET':
+        return render(request, 'core_application/about.html')
+
+    return render(request, 'core_application/about.html')
