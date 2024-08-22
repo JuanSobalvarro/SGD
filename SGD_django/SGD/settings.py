d609f03beb2ea5593847a342f2ac08a09df19ed9
QuesilloLover <adiliamoreno11@gmail.com>
2024-08-21 22:06:53 -0600

    :tada:

--- SGD_django/SGD/settings.py
+++ SGD_django/SGD/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'testing',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': 'josephjoestar18',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
